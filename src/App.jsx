f6aeb33bdaadd7b077390a3636f6f26a926e3837
Akmal Anfas <akmalanfas7@gmail.com>
2024-11-13 13:37:53 +0530

    project initilaized

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Dashboard from './page/dashboard/Dashboard.jsx';
+import Login from './page/login/Login.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      
+   <h1>hi ifham  </h1>
+
+   <Login />
+
+   <Dashboard />
+
+    </div>
+  );
+}
+
+export default App; 
