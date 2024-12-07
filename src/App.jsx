060196e9b621aae0f878558a270d12ed20a54b65
Akmal Anfas <akmalanfas7@gmail.com>
2024-12-07 20:11:29 +0530

    Login page developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import './App.css'
+import './App.css';
 import Dashboard from './pages/dashboard/Dashboard.jsx';
 import Login from './pages/login/Login.jsx';
+import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid';
+import { ChevronDownIcon } from '@heroicons/react/20/solid'; // Correct path for v20
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-   <h1>hi ifham  </h1>
-
-   <Login />
-
-   <Dashboard />
-   
-
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App; 
+export default App;
